e4dca96f92afd7d1f4ab5d81930ddcf967e12374
Katherine St. John <stjohn@lehman.cuny.edu>
2017-10-04 09:22:17 -0400

    Lecture 5 notes

---
+++ teaching/csci127/f17/months.py
@@ -0,0 +1,10 @@
+#CSci 127 Teaching Staff
+#October 2017
+#A program that uses functions to print out months.
+
+def monthString(monthNum):
+     monthString = ""
+
+     ### Fill in code here ###
+
+     return(monthString)
