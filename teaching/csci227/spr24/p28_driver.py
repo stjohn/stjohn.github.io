f3aa2ad53f10317e61fd2341f32434cfd942e52b
Katherine St. John <stjohn@lehman.cuny.edu>
2024-05-01 06:00:52 -0400

    updatingp28 driver

--- teaching/csci227/spr24/p28_driver.py
+++ teaching/csci227/spr24/p28_driver.py
@@ -5,11 +5,13 @@ from p28 import insert
 
 
 # Definition for a binary tree node.
+"""
 class TreeNode:
   def __init__(self, val=0, left=None, right=None):
     self.val = val
     self.left = left
     self.right = right
+"""
 
 
 
